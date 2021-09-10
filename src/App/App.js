167835f9af051e333694dce46472745f49c3adcf
Suruthika NR <suruthika.nr@skava.com>
2021-09-10 20:01:06 +0530

    Initial Commit

---
+++ src/App/App.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+function App() {
+  return <div> hello World!!! </div>
+}
+
+ReactDOM.render(<App />, document.getElementById('app-root'))
